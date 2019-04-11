274bf92b45dc760305298c7f551886b80b6a0517
Lucas Valladares <32403107+lucasnwvalladares@users.noreply.github.com>
2019-04-11 17:16:48 -0300

    chat

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 # from socket import *
+import time
 
 # Criar o socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,29 +16,42 @@ mensagem = "Ola Cliente"
 tamanho_da_mensagem = len(mensagem)
 print("Tamanho da mensagem = {}".format(len(mensagem)))
 
-while True:
+try:
     # Aguardar uma conexao
     print("Aguardando conexao")
-    connection, address_client = sock.accept()    
-    
+    connection, address_client = sock.accept()
+
     # Envio tamanho da mensagem
     connection.sendall(str(tamanho_da_mensagem).zfill(4).encode())
-    
+
     # Enviar mensagem
     connection.sendall(mensagem.encode())
+    while True:
+        # Aguardar tamanho da mensagem
+        expected_data_size = ''
+        
+        while(expected_data_size == ''):
+            expected_data_size += connection.recv(4).decode()
+            expected_data_size = int(expected_data_size)
+            received_data = ''
+        
+        while len(received_data) < expected_data_size:
+            # Ler o dado recebido
+            received_data += connection.recv(4).decode()
+            # print("Tamanho do dado {}".format(len(received_data)))
+            print(received_data)
 
-    # Aguardar tamanho da mensagem
-    expected_data_size = ''
-    while(expected_data_size == ''):
-        expected_data_size += connection.recv(4).decode()
-    expected_data_size = int(expected_data_size)
+        #
+        time.sleep(3)
 
-    received_data = ''
-    while len(received_data) < expected_data_size:
-        # Ler o dado recebido
-        received_data += connection.recv(4).decode()
-        print("Tamanho do dado {}".format(len(received_data)))
-    print(received_data)
+        resp = input("servidor: ").strip()
+        send_data_size = len(resp)
+        connection.sendall(str(send_data_size).zfill(4).encode())
+        connection.sendall(resp.encode())
 
-    # Finalizar a conexao
-    connection.close()
+        if received_data == 'see ya':
+            connection.close()
+
+finally:
+# Finalizar a conexao
+connection.close()
